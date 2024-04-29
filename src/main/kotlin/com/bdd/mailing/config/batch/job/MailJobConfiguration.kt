f0a8e1a6823f6c21e966f9013db310935c85d3f6
leesoobeen <02ggang9@gmail.com>
2024-04-29 14:12:08 +0900

    feat: mail batch writer 구현

--- src/main/kotlin/com/bdd/mailing/config/batch/job/MailJobConfiguration.kt
+++ src/main/kotlin/com/bdd/mailing/config/batch/job/MailJobConfiguration.kt
@@ -1,14 +1,18 @@
 package com.bdd.mailing.config.batch.job
 
+import com.bdd.mailing.domain.email.application.GmailService
 import com.bdd.mailing.domain.member.entity.Member
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
+import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.item.ItemReader
+import org.springframework.batch.item.ItemWriter
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
@@ -17,7 +21,8 @@ import javax.sql.DataSource
 @Configuration
 @EnableBatchProcessing
 class MailJobConfiguration(
-    private val dataSource: DataSource
+    private val dataSource: DataSource,
+    private val gmailService: GmailService,
 ) {
 
     @Bean
@@ -32,6 +37,7 @@ class MailJobConfiguration(
         return StepBuilder("sendMailStep", jobRepository)
             .chunk<Member, Member>(20, platformTransactionalManager)
             .reader(mailItemReader())
+            .writer(mailItemWriter(null, null))
             .build()
     }
 
@@ -44,6 +50,16 @@ class MailJobConfiguration(
             .rowMapper { rs, rowNum -> Member(rs.getLong(1), rs.getString(2), rs.getLong(3)) }
             .dataSource(dataSource)
             .build()
+    }
+
+    @Bean
+    @StepScope
+    fun mailItemWriter(
+        @Value("#{jobParameters[mailSubject]}") subject: String?,
+        @Value("#{jobParameters[mailMessage]}") mailMessage: String?,
+    ): ItemWriter<Member> = ItemWriter {memberChunk ->
+        val emails = memberChunk.items.map { it.email }.toTypedArray()
+        gmailService.sendEmail(emails, subject!!, mailMessage!!)
     }
 
 }
